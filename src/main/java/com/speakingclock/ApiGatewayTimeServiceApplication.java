e8a9f4becb22afcc8912cf8f6a17df9db38c725a
smohandos <suhaasmohandos@Suhaass-MacBook-Air.local>
2025-04-03 20:03:56 +0530

    Commiting API Gateway

---
+++ src/main/java/com/speakingclock/ApiGatewayTimeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.speakingclock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayTimeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayTimeServiceApplication.class, args);
+	}
+
+}
